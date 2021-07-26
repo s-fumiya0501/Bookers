ec018c3ddad2075395dedc4e97d94c3890a3670f
s_fumiya0501 <19t3058n@vc.ibaraki.ac.jp>
2021-07-26 18:30:25 +0000

    second change

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def index
+    
   end
 
   def show
+    
   end
 
   def new
+    @book=Book.new
   end
 
   def edit
+    
+  end
+  
+  def create
+    book = book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+  
+  private
+  
+  def blog_params
+    params.require(:book).permit(:title,:body)
   end
 end
