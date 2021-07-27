9180a9326935803137dff5b94ae448c1d0124d09
s_fumiya0501 <19t3058n@vc.ibaraki.ac.jp>
2021-07-27 10:46:15 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
   def index
-    
+    @books = Book.all
   end
 
   def show
-    
+    @book=Book.find(params[:id])
   end
 
   def new
@@ -12,18 +12,34 @@ class BooksController < ApplicationController
   end
 
   def edit
-    
+    @book=Book.find(params[:id])
   end
   
   def create
-    book = book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book=Book.new(book_params)
+    if book.save
+      redirect_to book_path(book.id),notice: 'Book was successfully created.'
+    else
+      render :create
+    end
+  end
+  
+    
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book),notice: 'Book was successfully updated.'
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path,notice: 'Book was successfully destroyed.'
   end
   
   private
   
-  def blog_params
-    params.require(:book).permit(:title,:body)
+  def book_params
+    params.permit(:title,:body)
   end
 end
